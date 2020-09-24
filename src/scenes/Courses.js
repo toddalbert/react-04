d6a8ce9f4116d1a17b2c8634bdb089699561b461
Todd <todd@bocacode.com>
2020-09-23 20:40:28 -0400

    rendering course list

--- src/scenes/Courses.js
+++ src/scenes/Courses.js
@@ -1,7 +1,37 @@
 import React from 'react'
 
-function Courses() {
-  return <h2>Short Courses</h2>
+class Courses extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      courses: [],
+    }
+  }
+  componentDidMount() {
+    let API_URL = 'https://bocanode.web.app/courses.json'
+    fetch(API_URL)
+      .then(response => response.json())
+      .then(data => this.setState({ courses: data }))
+  }
+  render() {
+    let { courses } = this.state
+    return (
+      <>
+        <h2>Short Courses</h2>
+        {(courses.length === 0)
+          ? <p>Loading...</p>
+          : <div className="course-section">
+            {/* I want to render a LIST && KEYS */}
+            {courses.map((aCourse, index) => {
+              return (
+                <div className="course" key={index}>{aCourse.title}</div>
+              )
+            })}
+          </div>
+        }
+      </>
+    )
+  }
 }
 
 export default Courses
--- src/scenes/Home.js
+++ src/scenes/Home.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
 function Home() {
-  return <h1>Welcome to DJAPOT Code School</h1>
+  return (
+    <>
+      <h1>Welcome to DJAPOT Code School</h1>
+    </>
+  )
+
 }
 
 export default Home
