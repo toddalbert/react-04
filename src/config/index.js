afd9d936b6de6944947c7bd72b167af565edff1b
Todd <todd@bocacode.com>
2020-09-25 18:34:56 -0400

    added - reset + counter buttons

---
+++ src/config/index.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyAvktdYF8oXSe3VzeVBFqP3mQAXPYTL6C4",
+  authDomain: "bocacode-react5.firebaseapp.com",
+  databaseURL: "https://bocacode-react5.firebaseio.com",
+  projectId: "bocacode-react5",
+  storageBucket: "bocacode-react5.appspot.com",
+  messagingSenderId: "382574107395",
+  appId: "1:382574107395:web:2ed1661288b526d411a736"
+}
--- src/scenes/Home.js
+++ src/scenes/Home.js
@@ -1,12 +1,15 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import Container from 'react-bootstrap/Container'
 import Button from 'react-bootstrap/Button'
+import ButtonGroup from 'react-bootstrap/ButtonGroup'
 
 function Home() {
   //     These go everywhere    This in top-level only
   const [counter, setCounter] = useState(0)
-
+  useEffect(() => {
+    document.title = `You clicked ${counter} times!`
+  }, [counter])
   return (
     <>
       <Jumbotron fluid>
@@ -15,11 +18,11 @@ function Home() {
           <p>
             We are the best React Devs around!
           </p>
-          <Button variant="primary" size="lg"
-            onClick={() => setCounter(counter + 1)}
-          >
-            Click Me
-          </Button>
+          <ButtonGroup>
+            <Button onClick={() => setCounter(counter - 1)}>-</Button>
+            <Button onClick={() => setCounter(0)}>Reset</Button>
+            <Button onClick={() => setCounter(counter + 1)}>+</Button>
+          </ButtonGroup>
           <p>You clicked the button {counter} times</p>
         </Container>
       </Jumbotron>
